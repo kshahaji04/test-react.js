d62ba08d1293a224079f2c88a3856333faf3f45f
Rushikesh Ahire <rushikesh@8848digital.com>
2023-09-25 12:05:15 +0530

    initial commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,2 @@
+// const Layout = ({children}) => {};
+// export default Layout;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,8 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from '../components/Header/Navbar';
+
+const AppRoutes = () => {
+  return <></>;
+};
+
+export default AppRoutes;
