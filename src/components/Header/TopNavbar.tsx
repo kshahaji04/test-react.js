03b5a445774e60316dba3b522bdb0e3fdcc3a80d
Rushikesh Ahire <rushikesh@8848digital.com>
2023-10-31 18:21:52 +0530

    header changeas

--- src/components/Header/TopNavbar.tsx
+++ src/components/Header/TopNavbar.tsx
@@ -1,33 +1,34 @@
 import UserProfile from './UserProfile';
 import '../../Style/Navbar.css';
 
-
 const TopNavbar = () => {
   return (
     <>
-      <nav className="container navbar navbar-light bg-light p-0">
-        <div className="container-fluid my-1">
-          {/* <NavLink className="navbar-brand" to="/master">
+      <div className="bg-light">
+        <nav className="container navbar navbar-light p-0">
+          <div className="container-fluid my-1">
+            {/* <NavLink className="navbar-brand" to="/master">
             <img src={logo} alt="" height="55px" width="auto" />
           </NavLink> */}
-          <div></div>
-          <div className="dropdown">
-            <button
-              className="btn navbar-toggler dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <a href="" className="text-right">
-                <i className="fa fa-user-circle" aria-hidden="true"></i>
-              </a>
-            </button>
-            <UserProfile />
+            <div></div>
+            <div className="dropdown">
+              <button
+                className="btn navbar-toggler dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <a href="" className="text-right">
+                  <i className="fa fa-user-circle" aria-hidden="true"></i>
+                </a>
+              </button>
+              <UserProfile />
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 };
