ab7af4e11d526e56e0f727e30618881a4919792b
rushikesh24 <rushikesh@8848digital.com>
2023-12-15 15:58:31 +0530

    production url changes

--- src/services/Config/api-config.ts
+++ src/services/Config/api-config.ts
@@ -1,2 +1,6 @@
-export const BASE_URL = 'https://shilpijewels.8848digitalerp.com';
+// export const BASE_URL = 'https://shilpijewels.8848digitalerp.com';
 // export const BASE_URL = 'http://192.168.29.54:8000';
+
+
+// PRODUCTION URL
+export const BASE_URL = 'https://prod-shilpijewels.8848digitalerp.com';
