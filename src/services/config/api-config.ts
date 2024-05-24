0568f20cbce5d7d6c87f2957a0dd8242cc4c34a7
rushikesh24 <rushikesh@8848digital.com>
2024-05-24 10:52:11 +0530

    fix/net wt is optional while scan QR code

--- src/services/config/api-config.ts
+++ src/services/config/api-config.ts
@@ -1,7 +1,6 @@
 // Staging URL
-
-export const BASE_URL = 'https://shilpijewels.8848digitalerp.com';
+// export const BASE_URL = 'https://shilpijewels.8848digitalerp.com';
 // export const BASE_URL = 'http://192.168.29.54:8000';
 
 // PRODUCTION URL
-// export const BASE_URL = 'https://prod-shilpijewels.8848digitalerp.com';
+export const BASE_URL = 'https://prod-shilpijewels.8848digitalerp.com';
