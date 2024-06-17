cd727723e30447d6df831e87ea2c322ec4b46527
rushikesh2404 <rushikesh@8848digital.com>
2024-06-17 12:33:38 +0530

    feat/new reports added

--- src/services/config/api-config.ts
+++ src/services/config/api-config.ts
@@ -1,17 +1,16 @@
 // Staging URL
-export const BASE_URL = 'https://shilpijewels.8848digitalerp.com';
+// export const BASE_URL = 'https://shilpijewels.8848digitalerp.com';
 // export const BASE_URL = 'http://192.168.29.54:8000';
 
 // PRODUCTION URL
-// export const BASE_URL = 'https://prod-shilpijewels.8848digitalerp.com';
-
+export const BASE_URL = 'https://prod-shilpijewels.8848digitalerp.com';
 
 export const headerGenerator = (token: any) => {
-    const API_CONFIG = {
-        headers: {
-            Accept: 'application/json',
-            Authorization: token,
-        },
-    };
-    return API_CONFIG;
+  const API_CONFIG = {
+    headers: {
+      Accept: 'application/json',
+      Authorization: token,
+    },
+  };
+  return API_CONFIG;
 };
