1da3d77eded1ad13d51db76cd31de9e2b90ef699
rushikesh2404 <rushikesh@8848digital.com>
2024-07-15 18:37:05 +0530

    fix:SR multiple receipt creation

--- src/components/SalesReturn/CreateSalesReturn/CreateSalesReturnMaster.tsx
+++ src/components/SalesReturn/CreateSalesReturn/CreateSalesReturnMaster.tsx
@@ -1,3 +1,5 @@
+import { useSelector } from 'react-redux';
+import { buttonLoadingState } from '../../../store/slices/btn-loading-slice';
 import SalesReturnTable from './SalesReturnTable';
 import SalesReturnTopSection from './SalesReturnTopSection';
 
@@ -16,14 +18,19 @@ const CreateSalesReturnMaster = ({
   topSectionInputData,
   handleCreateSR,
 }: any) => {
+  const buttonLoadingStateFromStore: any = useSelector(buttonLoadingState);
   return (
     <>
       <div className="d-flex justify-content-end">
         <button
           type="submit"
           onClick={handleCreateSR}
+          disabled={buttonLoadingStateFromStore?.loading}
           className="btn btn-outline-primary  px-2 py-0 form-submit-button"
         >
+          {buttonLoadingStateFromStore?.loading === true && (
+            <i className="fa fa-spinner me-1"></i>
+          )}
           Save
         </button>
       </div>
